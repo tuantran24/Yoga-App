fcd0d0ef6e4b43857b7c5dd0c4673c598f06cf55
kienbebong <kienpro2109@gmail.com>
2025-03-23 17:06:04 +0700

    add tip detail

--- app/src/main/java/com/example/yogaapp/FoodActivityDetail.java
+++ app/src/main/java/com/example/yogaapp/FoodActivityDetail.java
@@ -2,13 +2,28 @@ package com.example.yogaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FoodActivityDetail extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_detail);
+
+        textView = findViewById(R.id.txt);
+        String dstory = getIntent().getStringExtra("story");
+        textView.setText(dstory);
+    }
+
+    public void goback(View view) {
+        Intent intent = new Intent(FoodActivityDetail.this, FoodActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
