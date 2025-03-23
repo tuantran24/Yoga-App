1dada58816ba83e7e77628bd0488a85523b2287c
kienbebong <kienpro2109@gmail.com>
2025-03-23 08:44:22 +0700

    create git

---
+++ app/src/main/java/com/example/yogaapp/FoodActivityDetail.java
@@ -0,0 +1,14 @@
+package com.example.yogaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodActivityDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_detail);
+    }
+}
---
+++ app/src/main/java/com/example/yogaapp/SecondActivity2.java
@@ -0,0 +1,50 @@
+package com.example.yogaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class SecondActivity2 extends AppCompatActivity {
+
+    int[] newArray;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        newArray = new int[]{
+
+                R.id.bow_pose, R.id.bridge_pose, R.id.chair_pose, R.id.child_pose, R.id.cobbler_pose, R.id.cow_pose, R.id.playji_pose, R.id.pauseji_pose, R.id.crunches_pose,
+                R.id.situp_pose, R.id.rotation_pose, R.id.twist_pose, R.id.windmill_pose, R.id.legup_pose,
+
+        };
+
+    }
+
+    public void Imagebuttonclicked(View view) {
+
+        for(int i = 0; i < newArray.length; i++) {
+
+            if (view.getId() == newArray[i]) {
+
+                int value = i + 1;
+                Log.i("FIRST", String.valueOf(value));
+                Intent intent = new Intent(SecondActivity2.this, ThirdActivity2.class);
+                intent.putExtra("value", String.valueOf(value));
+                startActivity(intent);
+
+
+            }
+
+        }
+
+    }
+}
